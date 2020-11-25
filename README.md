595c033320846349d568846b7fa94b8b8e686c8f
Matteo Tossuto <matteotossuto@gmail.com>
2020-11-25 20:32:14 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+[![Build Status](https://travis-ci.org/BlackWolf00/Assignment-2.svg?branch=main)](https://travis-ci.org/BlackWolf00/Assignment-2)
+[![Coverage Status](https://coveralls.io/repos/github/BlackWolf00/Assignment-2/badge.svg?branch=main)](https://coveralls.io/github/BlackWolf00/Assignment-2?branch=main)
+
+# Assignment-2 di TOS
+
+Matteo Tossuto 1193493
---
+++ src/main/java/it/unipd/tos/App.java
@@ -0,0 +1,17 @@
+////////////////////////////////////////////////////////////////////
+// [Matteo] [Tossuto] [1193493]
+////////////////////////////////////////////////////////////////////
+
+package it.unipd.tos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
